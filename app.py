574dbbd56a21d71ffbc1a2bdbe2078e8d483dc77
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2021-05-22 10:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_mysqldb import MySQL
 
+
+
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -8,17 +10,29 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flaskdb'
 
 db_obj = MySQL(app)
+with app.app_context():
+    connect = db_obj.connection.cursor()
+    mySql_Create_Table_Query = """create table if not exists employee(employeeid int not null auto_increment, ename varchar(255) not null, designation varchar(255), phoneno varchar(12), primary key(employeeid));
+"""
+    connect.execute(mySql_Create_Table_Query)	
+
+#connect = db_obj.connection.cursor()
+
+
 
 @app.route('/')#Read
 def index():
     connect = db_obj.connection.cursor()
+     
     connect.execute("SELECT  * FROM employee")
     data = connect.fetchall()
     connect.close()
 
     return render_template('index.html', data=data )
 
-@app.route('/addemployee', methods = ['GET','POST']) #Create
+
+
+@app.route('/addemployee', methods = ['GET','POST'])#Create
 def addemployee():
     
     if request.method == "POST":
@@ -34,6 +48,8 @@ def addemployee():
      
     else:
      	return render_template('addemployee.html')
+
+
 
 @app.route('/update/<int:employeeid>', methods = ['GET','POST'])#Update
 def update(employeeid):
@@ -65,6 +81,7 @@ def deleteemployee(employeeid):
         db_obj.connection.commit()
 
         return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
